e6d85578926f9c7827d834d238d7138dc51f10ae
mala23 <mail@mala23.me>
2014-10-19 11:35:18 +0100

    Can log list of names to console, for debugging

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ var express     = require('express'),
     bodyParser  = require('body-parser'),
     app         = express();
 
+function log(req, res, next) {
+    console.log(names);
+    next();
+};
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var names = [];
+
+app.all('/', log, function(req, res, next) {
+    console.log('From all'); 
+    next();
+});
 
 app.get('/', function(req, res) {
     res.render('index.jade', { names: names });
